761864d31299efc07a2ce052a0237b4bf653322a
Tao Lu <lutao_0724@hotmail.com>
2019-10-23 14:09:27 +1100

    background sync try

--- app/src/main/java/com/comp90018/H1Calendar/WiFiAutoSync.java
+++ app/src/main/java/com/comp90018/H1Calendar/WiFiAutoSync.java
@@ -9,6 +9,7 @@ import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.IBinder;
+import android.os.Looper;
 import android.os.Message;
 import android.util.Log;
 import android.widget.Toast;
@@ -23,6 +24,9 @@ import com.comp90018.H1Calendar.utils.ResultFromSync;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
@@ -44,6 +48,7 @@ public class WiFiAutoSync extends Service {
 
     // variable used to store user info that get from shared preferences
     private String userToken, userId, userEmail, userName, userPwd;
+
 
 
     @Nullable
@@ -83,14 +88,13 @@ public class WiFiAutoSync extends Service {
                         loadUserInfo();
                         String userid = userId;
                         String usertoken = userToken;
-                        Log.e("background service", "====run=======" + userid + "=======");
-                        Log.e("background service", "===========runnable=======");
 
-                        if(!userid.equals("")){
-                            // sync
-                            // syncToCloud();
-                            syncToCloud(usertoken, userid);
-                            updateUI.sendEmptyMessage(0);
+                        Log.e("background service", "===========runnable=======");
+                        //updateUI.sendEmptyMessage(0);
+
+                        if(!userid.equals("") && !usertoken.equals("")){
+
+                            //updateUI.sendEmptyMessage(0);
                         }
 
 
@@ -174,6 +178,7 @@ public class WiFiAutoSync extends Service {
         String jsonObject = gson1.toJson(eventSync);
         String urlAddress = "http://35.197.167.33:8222/sync";
         Log.d("sendJson", jsonObject);
+        Looper.prepare();
         Handler handler = new Handler() {
             public void handleMessage(Message msg) {
                 super.handleMessage(msg);
@@ -182,16 +187,17 @@ public class WiFiAutoSync extends Service {
                 ResultFromSync json = gson.fromJson(jsonString, ResultFromSync.class);
                 //register success, login
                 if (json.code == 201) {
+                    Log.e("background service", "===========sync=======");
                     saveUserInfo(json.token, json.userInfo.userid,json.userInfo.email,json.userInfo.username);
                     loadUserInfo();
-                    updateEventAndLocationFromSync(userId, json.events, json.locations);
+                    updateEventAndLocationFromSync(userId, json.events, json.locations, dbhelper);
                     dbhelper.deleteEventByEventIdForReal();
                     dbhelper.deleteLocationByLocationIdForReal();
 
                     Toast.makeText(getApplicationContext(), "Sync success",
                             Toast.LENGTH_SHORT).show();
                 } else if (json.code == 408) {
-                    jumpToNavigationHeaderLogin();
+                    //jumpToNavigationHeaderLogin();
                     Toast.makeText(getApplicationContext(), json.msg,
                             Toast.LENGTH_SHORT).show();
                 } else {
@@ -228,6 +234,56 @@ public class WiFiAutoSync extends Service {
         //}
 
 
+    }
+
+    public void updateEventAndLocationFromSync(String userid, List<Event> events, List<Location> locations, sqliteHelper dbhelper){
+
+        List<Event> localEvents = dbhelper.syncGetAllEventsByUserId(userid);
+        HashMap<String, Event> eventMap = new HashMap<String, Event>();
+        //Log.d("update", "update");
+        for (Event localevent: localEvents){
+            eventMap.put(localevent.eventid, localevent);
+
+        }
+        for (Event event: events){
+            if (event.isdelete != null && event.isdelete.compareTo("T") == 0){
+                if (eventMap.get(event.eventid) != null) {
+                    dbhelper.deleteEventByEventId(event.eventid);
+                }
+            }
+            if (eventMap.get(event.eventid) == null) {
+                dbhelper.insertSyncEvent(event);
+            }
+            else{
+                SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                Event localEvent = eventMap.get(event.eventid);
+                if(event.updatetime != null && localEvent.updatetime != null){
+                    try{
+                        if (simpleDateFormat.parse(event.updatetime).getTime() > simpleDateFormat.parse(localEvent.updatetime).getTime()){
+                            dbhelper.updateEventFromSyncByEventId(event.eventid, event);
+                        }
+                    }catch(ParseException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+
+        List<Location> localLocations = dbhelper.syncGetAllLocationsByUserId(userid);
+        HashMap<String, Location> locationMap = new HashMap<String, Location>();
+        for (Location locallocation: localLocations){
+            locationMap.put(locallocation.locationid, locallocation);
+        }
+        for (Location location: locations){
+            if (location.isdelete != null && location.isdelete.compareTo("T") == 0){
+                if (locationMap.get(location.locationid) != null) {
+                    dbhelper.deleteLocationByLocationId(location.locationid);
+                }
+            }
+            if (locationMap.get(location.locationid) == null) {
+                dbhelper.insertSyncLocation(location);
+            }
+        }
     }
 
     @Override
